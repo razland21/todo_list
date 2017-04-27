e42e2ba6719c09a719c2fb3e0150163862f6bd56
Rica <razarcon@gmail.com>
2017-04-27 14:47:52 -0700

    can indicate where item should be added

--- todo_list.py
+++ todo_list.py
@@ -9,11 +9,21 @@ You can run the script in your Terminal at any time using the command:
 
 """
 
-def add_to_list(my_list):
-    """Takes user input and adds it as a new item to the end of the list."""
-
-    list_item = raw_input("What would you like to add to the list? ")
-    my_list.append(list_item)
+def add_to_list(my_list, location=1):
+	"""Takes user input and adds it as a new item to the end of the list."""
+	
+	list_item = raw_input("What would you like to add to the list? ")
+	if location.isdigit():
+		location = int(location)
+		if location <= len(my_list):
+			my_list.insert(location-1, list_item)
+		else:
+			print "That location is larger than the number of items in the list! Adding task to end of list."
+			my_list.append(list_item)
+	else:
+		if location != "last":
+			print "That was not a valid position. Adding task to end of list."
+		my_list.append(list_item)
 
 #    print my_list
 #    return my_list
@@ -26,11 +36,14 @@ def view_list(my_list):
         print str(my_list.index(item)+1) + ". " + item
 
 def delete_first_item(my_list):
-    print "You are deleting " + my_list[0]
-    del my_list[0]
+	if len(my_list) == 0:
+		print "The list is already empty."
+	else:
+		print "You are deleting " + my_list[0]
+		del my_list[0]
 
 
-def display_main_menu(my_list1):
+def display_main_menu(my_list):
     """Displays main options, takes in user input, and calls view or add function."""
 
     user_options = """
@@ -47,11 +60,12 @@ def display_main_menu(my_list1):
         user_input = raw_input(">>> ").upper()
 
         if user_input == "A":
-            add_to_list(my_list1)
+			location = raw_input("What order in the list should this be? Put 1 for first item and last for last item: ")
+			add_to_list(my_list, location.strip())
         elif user_input == "B":
-            view_list(my_list1)
+            view_list(my_list)
         elif user_input == "C":
-            delete_first_item(my_list1)
+            delete_first_item(my_list)
         elif user_input == "D":
             break
         else:
